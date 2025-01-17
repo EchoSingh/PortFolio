b8dc240fc40a34bce3de029e945dbe8ca78b7cb7
EchoSingh <adityarathore2606@gmail.com>
2025-01-17 23:24:52 +0530

    :)

--- src/components/ui/SocialLinks.tsx
+++ src/components/ui/SocialLinks.tsx
@@ -1,4 +1,5 @@
 import { FaLinkedin, FaGithub, FaTwitter, FaDiscord, FaYoutube } from 'react-icons/fa';
+import { FaBluesky } from "react-icons/fa6";
 import { SiHuggingface } from "react-icons/si";
 const socialLinks = [
   {
@@ -11,6 +12,12 @@ const socialLinks = [
     name: 'Hugging Face',
     url: 'https://huggingface.co/adi2606',
     icon: SiHuggingface,
+    color: 'text-blue-400',
+  },
+  {
+    name: 'Bluesky',
+    url: 'https://bsky.app/profile/adi-r.bsky.social',
+    icon: FaBluesky,
     color: 'text-blue-400',
   },
   {
@@ -27,16 +34,16 @@ const socialLinks = [
   },
   {
     name: 'Youtube',
-    url: 'https://www.youtube.com/@RealWorldC-zk5gj',
+    url: 'https://www.youtube.com/@adi.s.r',
     icon: FaYoutube,
     color: 'text-indigo-400',
   },
   {
     name: 'Twitter',
-    url: 'https://x.com/ADITYAS01117542',
+    url: 'https://x.com/a_d_i_s_r',
     icon: FaTwitter,
     color: 'text-blue-400',
-  },
+  }
 ];
 
 export default function SocialLinks() {
