0dd1657788573b1044d3865271a47d659f3ffada
EchoSingh <adityarathore2606@gmail.com>
2025-01-18 14:27:57 +0530

    ^_^

--- src/components/ui/SocialLinks.tsx
+++ src/components/ui/SocialLinks.tsx
@@ -1,5 +1,4 @@
-import { FaLinkedin, FaGithub, FaTwitter, FaDiscord, FaYoutube } from 'react-icons/fa';
-import { FaBluesky } from "react-icons/fa6";
+import { FaLinkedin, FaGithub, FaTwitter, FaDiscord, FaYoutube, FaBluesky } from 'react-icons/fa6';
 import { SiHuggingface } from "react-icons/si";
 const socialLinks = [
   {
